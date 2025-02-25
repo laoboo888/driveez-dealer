066b41ecfbb6d0e814b1794890957bead673c4c0
laoboo888 <limlaoboo888@gmail.com>
2025-02-25 21:27:47 +0800

    Update settings.py

--- CarDealerWeb/cardealer/settings.py
+++ CarDealerWeb/cardealer/settings.py
@@ -143,10 +143,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')  # Ensure this path is correct
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'cardealer/static'),
+    os.path.join(BASE_DIR, 'cardealer/static'),  # Add paths where your static files are stored
 ]
 
 # Media settings
